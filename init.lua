299d20b4f24eae34c4f31bbff859c8630e985052
TopengDev <topengdev@outlook.com>
2025-11-19 21:30:06 +0700

    :hammer::zap: ~ Stability update for windows 10 setup with wsl & alacritty

--- init.lua
+++ init.lua
@@ -26,6 +26,25 @@ vim.g.loaded_netrwFileHandlers = 1
 -- Faster startup
 vim.loader.enable()
 
+-- WSL Windows clipboard support
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    name = 'WslClipboard',
+    copy = {
+      ['+'] = 'clip.exe',
+      ['*'] = 'clip.exe',
+    },
+    paste = {
+      ['+'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ['*'] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+    cache_enabled = 0,
+  }
+end
+
+-- Use system clipboard
+vim.opt.clipboard = "unnamedplus"
+
 -- Enable syntax highlighting and filetype detection
 vim.cmd("syntax enable")
 vim.cmd("filetype plugin indent on")
